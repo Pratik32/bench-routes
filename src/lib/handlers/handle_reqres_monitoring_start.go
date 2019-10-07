ed892885667056da738ef5a3a9e75c586488b71b
Aquib Baig <aquibbaig97@gmail.com>
2019-10-07 13:08:32 +0530

    Configure handler for req-res-monitoring
    Signed-off-by: Aquib Baig <aquibbaig97@gmail.com>

---
+++ src/lib/handlers/handle_reqres_monitoring_start.go
@@ -0,0 +1,57 @@
+package handlers
+
+import (
+	"log"
+	"sync"
+	"time"
+
+	"github.com/zairza-cetb/bench-routes/src/lib/modules/response"
+	"github.com/zairza-cetb/bench-routes/src/lib/utils"
+)
+
+// HandleReqResMonitoringStart handle the route "start"
+func HandleReqResMonitoringStart(config utils.YAMLBenchRoutesType, reqResMonitoringServiceState string) {
+	routes := config.Config.Routes
+	monitoringInterval := getInterval(config.Config.Interval, "req-res-deplay-and-monitoring")
+	if monitoringInterval == (testInterval{}) {
+		log.Fatalf("interval not found in configuration file for req-res monitoring")
+		return
+	}
+
+	for _, route := range routes {
+		i := 0
+		for {
+			i++
+			config = config.Refresh()
+
+			switch config.Config.UtilsConf.ServicesSignal.ReqResDelayMonitoring {
+			case "active":
+				var wg sync.WaitGroup
+				wg.Add(len(routes))
+				go response.HandleResponseDelayForRoute(route, &wg)
+
+				wg.Wait()
+			case "passive":
+				// terminate the goroutine
+				log.Printf("terminating req-res monitoring goroutine\n")
+				return
+			default:
+				log.Fatalf("invalid service-state value of req-res monitoring\n")
+				return
+			}
+
+			intrv := time.Duration(monitoringInterval.duration)
+			switch monitoringInterval.ofType {
+			case "hr":
+				time.Sleep(intrv * time.Hour)
+			case "min":
+				time.Sleep(intrv * time.Minute)
+			case "sec":
+				time.Sleep(intrv * time.Second)
+			default:
+				log.Fatalf("invalid interval-type for req-res monitoring\n")
+				return
+			}
+		}
+	}
+}
